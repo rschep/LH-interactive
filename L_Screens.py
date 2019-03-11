36dd46e409f7b3691960a2e6187aac409c8d82bf
mcptje <reekmans.jeroen@gmail.com>
2019-03-11 15:34:19 +0100

    fancier selection effect in function

--- L_Screens.py
+++ L_Screens.py
@@ -1,10 +1,15 @@
 # import the necessary packages
 import sys, pygame
 import simpleaudio as sa
+from pygame import gfxdraw
 
 #Variable
 audioPlay = False
 play_obj = sa.PlayObject(1)
+
+selectionSize1 = 30
+selectionSize2 = 60
+selectionSize3 = 64
 
 # Dev purposes
 def func_DEV(screen, center):
@@ -34,10 +39,34 @@ def func_testImage(screen):
 
 	
 # Ready code
-def func_drawCenter(screen, center, dotColor, radius, width):
+def func_drawCenter(screen, center, dotColor):
 	x = center[0]
 	y = center[1]
-	pygame.draw.circle(screen,(dotColor),(x,y), radius)
+	pygame.draw.circle(screen,(dotColor),(x,y), selectionSize1)
+	
+def func_clickAnimation(screen, x, y):
+	global selectionSize1, selectionSize2, selectionSize3
+	selectionSize1 -=1
+	selectionSize2 -=2
+	selectionSize3 -=2
+	# Check negatives
+	if selectionSize1 <= 1:
+		selectionSize1 = 1
+	if selectionSize2 <= 1:
+		selectionSize2 = 1
+	if selectionSize3 <= 1:
+		selectionSize3 = 1
+		return True 		# Animation done
+		
+	pygame.gfxdraw.circle(screen, x, y, selectionSize2, (255,255,255))
+	pygame.gfxdraw.circle(screen, x, y, selectionSize3, (255,255,255))	
+	return False
+
+def func_resetAnimation():
+	global selectionSize1, selectionSize2, selectionSize3
+	selectionSize1 = 30
+	selectionSize2 = 60
+	selectionSize3 = 64
 	
 def func_PlaySound(path):
 	global audioPlay, play_obj
